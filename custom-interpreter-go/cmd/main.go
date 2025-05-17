0e349311e33d091911ec7d72ea0247e6ccfa4ad8
Vasu Sheoran <vasusheoran92@gmail.com>
2025-05-18 03:40:31 +0530

    minor fix to custom interpreter

--- custom-interpreter-go/cmd/main.go
+++ custom-interpreter-go/cmd/main.go
@@ -4,16 +4,10 @@ import (
 	"custom-interpreter-go/pkg/repl"
 	"fmt"
 	"os"
-	"os/user"
 )
 
 func main() {
-	user, err := user.Current()
-	if err != nil {
-		panic(err)
-	}
-	fmt.Printf("Hello %s! This is the Banana programming language!\nFeel free to type in commands. Type `exit` command to exit.\n",
-		user.Username)
+	fmt.Printf("Hello! This is the Banana programming language!\nFeel free to type in commands. Type `exit` command to exit.\n")
 	repl.Start(os.Stdin, os.Stdout)
 	fmt.Printf("Goodbye!\n")
 }
